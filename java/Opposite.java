f7a0d7fcd0a9255a1f2a851d8a0066f38feb2d77
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2025-04-02 23:09:27 +0700

    create Opposite.java

---
+++ java/Opposite.java
@@ -0,0 +1,14 @@
+/**
+ * Tanggal: 2 April 2025
+ * Link: https://www.codewars.com/kata/56dec885c54a926dcd001095/train/java
+ */
+
+public class Opposite {
+    public static void main(String[] args) {
+        System.out.println(opposite(1));
+    }
+
+    public static int opposite(int number) {
+        return -number;
+    }
+}
