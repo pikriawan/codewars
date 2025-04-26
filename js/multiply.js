e36e8d7e84388300a60fc80428032b19ba077e38
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2025-04-26 22:52:01 +0700

    create multiply.js

---
+++ js/multiply.js
@@ -0,0 +1,10 @@
+/**
+ * Tanggal: 26 April 2025
+ * Link: https://www.codewars.com/kata/50654ddff44f800200000004/train/javascript
+ */
+
+function multiply(a, b) {
+    return a * b;
+}
+
+console.log(multiply(2, 2));
