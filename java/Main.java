508964076fc40f4d5f911dac76f69e4ebc03c4bd
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2025-12-03 10:14:20 +0700

    delete: Main.java

--- java/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String str = "";
-
-        if (!str) {
-            System.out.println("String is falsy");
-        }
-    }
-}
