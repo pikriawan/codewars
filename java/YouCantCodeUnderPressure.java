82121bbc5d83757477d84e99d3e32f437979e0c0
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2025-12-31 23:06:57 +0700

    create: YouCantCodeUnderPressure.java

---
+++ java/YouCantCodeUnderPressure.java
@@ -0,0 +1,16 @@
+/**
+ * Tanggal: 31 Desember 2025
+ * Link: https://www.codewars.com/kata/53ee5429ba190077850011d4/train/java
+ */
+
+public class YouCantCodeUnderPressure {
+    public static void main(String[] args) {
+        System.out.println(doubleInteger(2));
+        System.out.println(doubleInteger(3));
+        System.out.println(doubleInteger(7));
+    }
+
+    public static int doubleInteger(int i) {
+        return i * 2;
+    }
+}
