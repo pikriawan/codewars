978b0027fe90c0ca2e508908dfdedd8afe653b89
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2026-03-08 20:09:06 +0700

    create: bouncing-ball.c

---
+++ c/bouncing-ball.c
@@ -0,0 +1,52 @@
+/**
+ * Tanggal: 8 Maret 2026
+ * Link: https://www.codewars.com/kata/5544c7a5cb454edb3c000047/train/c
+ */
+
+#include <stdio.h>
+
+int bouncing_ball(double h, double bounce, double window) {
+    if (h <= 0) {
+        return -1;
+    }
+
+    if (bounce <= 0 || bounce >= 1) {
+        return -1;
+    }
+
+    if (window >= h) {
+        return -1;
+    }
+
+    if (h <= 0) {
+        return -1;
+    }
+
+    if (bounce <= 0 || bounce >= 1) {
+        return -1;
+    }
+
+    if (window >= h) {
+        return -1;
+    }
+
+    int count = 1;
+    double remaining = h * bounce;
+
+    while (remaining > window) {
+        count += 2;
+        remaining = remaining * bounce;
+    }
+
+    return count;
+}
+
+int main() {
+    double h = 2;
+    double bounce = 0.5;
+    double window = 1;
+
+    printf("%d\n", bouncing_ball(h, bounce, window));
+
+    return 0;
+}
