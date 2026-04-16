52ba242800e9d218f7de40a2a0de53a93116a8c6
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2026-04-16 12:19:41 +0700

    create: get-character-from-number.py

---
+++ python/get-character-from-number.py
@@ -0,0 +1,10 @@
+# Tanggal: 16 April 2026
+# Link: https://www.codewars.com/kata/55ad04714f0b468e8200001c/train/python
+
+def get_character_from_number(number):
+    return chr(number)
+
+if __name__ == "__main__":
+    print(get_character_from_number(65))
+    print(get_character_from_number(97))
+    print(get_character_from_number(48))
