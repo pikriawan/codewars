676b705c3e6fab3c0c6b65af7eb42b47b5629b41
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2026-04-25 21:48:38 +0700

    create: plural.py

---
+++ python/plural.py
@@ -0,0 +1,9 @@
+# Tanggal: 25 April 2026
+# Link: https://www.codewars.com/kata/52ceafd1f235ce81aa00073a/train/python
+
+def plural(n):
+    return n != 1
+
+if __name__ == '__main__':
+    print(plural(1))
+    print(plural(2))
