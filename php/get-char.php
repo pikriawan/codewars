cbd4883602fcd66596467177dbb4c1f483621885
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2026-07-14 18:23:37 +0700

    create: get-char.php

---
+++ php/get-char.php
@@ -0,0 +1,11 @@
+<?php
+
+# Tanggal: 14 Juli 2026
+# Link: https://www.codewars.com/kata/55ad04714f0b468e8200001c/train/php
+
+function get_char($code) {
+    return chr($code);
+}
+
+echo get_char(65) . PHP_EOL;
+echo get_char(97) . PHP_EOL;
