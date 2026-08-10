1a9195cd402f989adb3694fab8238fa2d8510ca0
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2026-08-10 22:21:59 +0700

    create: RowSumOddNumbers.java

---
+++ java/RowSumOddNumbers.java
@@ -0,0 +1,14 @@
+/**
+ * Tanggal: 10 Agustus 2026
+ * Link: https://www.codewars.com/kata/55fd2d567d94ac3bc9000064/train/java
+ */
+
+public class RowSumOddNumbers {
+    public static void main(String[] args) {
+        System.out.println(rowSumOddNumbers(42));
+    }
+
+    public static int rowSumOddNumbers(int n) {
+        return n * n * n;
+    }
+}
