499fb9bc6484f1f0e311a38035394600089e7e77
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2026-09-12 22:04:18 +0700

    create: row-sum-odd-numbers.js

---
+++ js/row-sum-odd-numbers.js
@@ -0,0 +1,10 @@
+/**
+ * Tanggal: 12 September 2026
+ * Link: https://www.codewars.com/kata/55fd2d567d94ac3bc9000064/train/javascript
+ */
+
+function rowSumOddNumbers(n) {
+    return n * n * n;
+}
+
+console.log(rowSumOddNumbers(42));
