ff5bbedcee7d0633dc33e8cb18ba62aa519a3769
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2026-09-24 19:34:36 +0700

    create: square.js

---
+++ js/square.js
@@ -0,0 +1,10 @@
+/**
+ * Tanggal: 24 September 2026
+ * Link: https://www.codewars.com/kata/523b623152af8a30c6000027/train/javascript
+ */
+
+function square(n) {
+    return n * n;
+}
+
+console.log(square(3));
