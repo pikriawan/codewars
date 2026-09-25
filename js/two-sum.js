7bf5bcddf70adeec4ec5acd23e4fd323c1a62e85
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2026-09-25 20:27:23 +0700

    create: two-sum.js

---
+++ js/two-sum.js
@@ -0,0 +1,16 @@
+/**
+ * Tanggal: 25 September 2026
+ * Link: https://www.codewars.com/kata/52c31f8e6605bcc646000082/train/javascript
+ */
+
+function twoSum(numbers, target) {
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+            if (numbers[i] + numbers[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+}
+
+console.log(twoSum([1, 2, 3], 4));
